f2c5fbbe2749bc401983adb654e3cf2e24d6a89c
Priyanshu61103 <priyanshuparashar108@gmail.com>
2026-02-16 20:54:02 +0530

    sixth commit

--- backend/dbconfig.js
+++ backend/dbconfig.js
@@ -1,6 +1,6 @@
 import { MongoClient } from "mongodb";
 
-const url = "mongodb+srv://priyanshuparashar108:Priyanshuparashar6@cluster0.rkiizre.mongodb.net/?appName=Cluster0";
+const url = "mongodb+srv://priyanshuparashar108:<db_password>@cluster0.rkiizre.mongodb.net/?appName=Cluster0+srv://priyanshuparashar108:Priyanshuparashar6@cluster0.rkiizre.mongodb.net/?appName=Cluster0";
 const dbName = "node-project";
 export const collectionName = "todo";
 const client = new MongoClient(url);
